ae035feed315410586785591a309c5f87287f102
duong Viet <duongviet@duongs-mbp.lan>
2020-10-15 11:31:27 +0700

    Update Add Booking form

--- amplify-with-angular/src/app/add-booking/add-booking.component.ts
+++ amplify-with-angular/src/app/add-booking/add-booking.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Auth } from 'aws-amplify';
 
 import { APIService } from '../API.service';
 import { Booking } from '../../type/booking';
@@ -19,18 +20,26 @@ export class AddBookingComponent implements OnInit {
   };
   roomId: string;
   buildingId: string;
+  userId: string;
 
   bookingForm: FormGroup;
 
   informMsg: string = '';
 
+  isSubmitted: boolean = false;
+
   constructor(
     private activatedroute: ActivatedRoute,
     private api: APIService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+    Auth.currentUserInfo().then(data => {
+      this.userId = data.username || '';
+    });
+
     this.buildingId = this.activatedroute.snapshot.paramMap.get('buildingId');
     this.roomId = this.activatedroute.snapshot.paramMap.get('roomId');
 
@@ -43,7 +52,6 @@ export class AddBookingComponent implements OnInit {
         this.bookingForm = this.fb.group({
           buildingId: [data.buildingId, Validators.required],
           roomId: [data.roomId, Validators.required],
-          userId: ['', Validators.required],
           startTime: ['', Validators.required],
           endTime: ['', Validators.required]
         })
@@ -52,21 +60,31 @@ export class AddBookingComponent implements OnInit {
   }
 
   addBooking() {
-    if (this.bookingForm.valid) {
-      const newBooking: Booking = {
-        buildingId: this.bookingForm.value.buildingId,
-        roomId: this.bookingForm.value.roomId,
-        userId: this.bookingForm.value.userId,
-        startTime: this.bookingForm.value.startTime,
-        endTime: this.bookingForm.value.endTime
-      };
+    this.isSubmitted = true;
+    if (this.bookingForm.valid && this.userId) {
+      const startTimeMili = new Date(this.bookingForm.value.startTime).getTime();
+      const endTimeMili = new Date(this.bookingForm.value.endTime).getTime();
 
-      this.api.CreateBookingRoom(newBooking).then(data => {
-        this.informMsg = 'Added!'
-      },
-      err => {
-        this.informMsg = `Error! ${err.errors[0].message}`;
-      });
+      if (startTimeMili >= endTimeMili) {
+        this.informMsg = 'Start Time need smaller then End Time!';
+      } else {
+        this.informMsg = '';
+
+        const newBooking: Booking = {
+          buildingId: this.bookingForm.value.buildingId,
+          roomId: this.bookingForm.value.roomId,
+          userId: this.userId,
+          startTime: startTimeMili.toString(),
+          endTime: endTimeMili.toString()
+        };
+
+        this.api.CreateBookingRoom(newBooking).then(data => {
+          this.router.navigate(['/booking']);
+        },
+        err => {
+          this.informMsg = `Error! ${err.errors[0].message}`;
+        });
+      }
     }
   }
 }
