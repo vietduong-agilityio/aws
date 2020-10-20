337959dc5ce37a2820fd7540ffb290511b8d6cdd
duong Viet <duongviet@duongs-mbp.lan>
2020-10-20 09:21:55 +0700

    Delete a single Booking

--- amplify-with-angular/src/app/booking/booking.component.ts
+++ amplify-with-angular/src/app/booking/booking.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { APIService } from '../API.service';
 import { Booking } from '../../type/booking';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-booking',
@@ -11,11 +13,12 @@ export class BookingComponent implements OnInit {
 
   bookingList: Booking[] = [];
 
-  displayedColumns: string[] = ['bookingId', 'buildingId', 'roomId', 'startTime', 'endTime', 'userId'];
+  displayedColumns: string[] = ['bookingId', 'buildingId', 'roomId', 'startTime', 'endTime', 'userId', 'action'];
 
   constructor(
     private api: APIService,
-    private cd: ChangeDetectorRef
+    private cd: ChangeDetectorRef,
+    public dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
@@ -30,6 +33,23 @@ export class BookingComponent implements OnInit {
         this.cd.detectChanges();
       }
     })
+  }
+
+  openDialog(bookingId, userId) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: 'Do you want to delete this Booking? You can not reverse this action!'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.api.DeleteBooking({
+          userId: userId,
+          bookingId: bookingId
+        }).then(data => {
+          this.bookingList = this.bookingList.filter(item => item.bookingId !== bookingId);
+        })
+      }
+    });
   }
 
 }
